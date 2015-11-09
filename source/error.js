a6106693adb045bd84f805b22af10b9d07868d4f
Dominik Guzei <dominik.guzei@gmail.com>
2015-11-09 11:57:47 +0100

    allow error classes to define default messages
    This is handy for a general purpose description of
    the error when nothing special is necessary but
    more context still makes sense ;-)

--- source/error.js
+++ source/error.js
@@ -2,7 +2,7 @@ Space.Object.extend(Space, 'Error', _.extend({}, Error.prototype, {
   Constructor: function() {
     var tmp = Error.apply(this, arguments);
     tmp.name = this.name = this.constructor.name;
-    this.message = tmp.message;
+    if(tmp.message) this.message = tmp.message;
     this.stack = tmp.stack;
   }
 }));
