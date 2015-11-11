ea51b52a52de373a9b016a9d9ef7af7232b18d63
Dominik Guzei <dominik.guzei@gmail.com>
2015-11-11 14:39:10 +0100

    improves test messages and comments

--- tests/integration/requiring-modules.tests.js
+++ tests/integration/requiring-modules.tests.js
@@ -1,9 +1,10 @@
 
 describe("Space.base - Requiring modules in other modules and apps", function(){
 
-  it("can have multiple implementing modules required by an Application", function(){
+  it("multiple modules should be able to require the same base module", function(){
 
     Space.Module.define('BaseModule', {
+      // Regression test -> this was invoked twice at some point
       afterInitialize: function() {
         this.injector.map('x').to('y');
       }
@@ -15,7 +16,7 @@ describe("Space.base - Requiring modules in other modules and apps", function(){
     var MyApp = Space.Application.define('MyApp', {
       RequiredModules: ['DependentModule1', 'DependentModule2']
     });
-    
+
     function appInit() { new MyApp(); }
     expect(appInit).to.not.throw(Error);
   });
