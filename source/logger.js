ff37463d38fef3061033e14135c02ef27ba7826f
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-11-19 18:54:06 +1100

    Switch from sysLog to log, add minLevel config and ENV
    Changes:
    - SPACE_LOG_ENABLED replaces SPACE_SYSLOG_ENABLED,
    - SPACE_APPLOG_ENABLED removed from spec
    Additions
    - SPACE_LOG_MIN_LEVEL restricts logging to types above one of the
    following: debug, info, notice, warning, error, crit, alert, emerg

--- source/logger.js
+++ source/logger.js
@@ -1,3 +1,5 @@
+let config = Space.configuration;
+
 if (Meteor.isServer) {
   winston = Npm.require('winston');
 }
@@ -5,10 +7,10 @@ if (Meteor.isServer) {
 Space.Logger = Space.Object.extend(Space, 'Logger', {
 
   _logger: null,
+  _minLevel: 6,
   _state: 'stopped',
 
   Constructor() {
-
     if (Meteor.isServer) {
       this._logger = new (winston.Logger)({
         transports: [
@@ -22,6 +24,16 @@ Space.Logger = Space.Object.extend(Space, 'Logger', {
     }
     if (Meteor.isClient) {
       this._logger = console;
+    }
+  },
+
+  setMinLevel(name) {
+    let newCode = this._levelCode(name)
+    if (this._minLevel !== newCode) {
+      this._minLevel = newCode;
+      if (Meteor.isServer) {
+        this._logger.transports.Console.level = name;
+      }
     }
   },
 
@@ -39,45 +51,59 @@ Space.Logger = Space.Object.extend(Space, 'Logger', {
 
   debug(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.debug.apply(this, arguments);
+    if (this._isRunning()) this._logger.debug.apply(this, arguments);
   },
 
   info(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.info.apply(this, arguments);
+    if (this._isRunning()) this._logger.info.apply(this, arguments);
   },
 
   notice(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.notice.apply(this, arguments);
+    if (this._isRunning()) this._logger.notice.apply(this, arguments);
   },
 
   warning(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.warning.apply(this, arguments);
+    if (this._isRunning()) this._logger.warning.apply(this, arguments);
   },
 
   error(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.error.apply(this, arguments);
+    if (this._isRunning()) this._logger.error.apply(this, arguments);
   },
 
   crit(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.crit.apply(this, arguments);
+    if (this._isRunning()) this._logger.crit.apply(this, arguments);
   },
 
   alert(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.alert.apply(this, arguments);
+    if (this._isRunning()) this._logger.alert.apply(this, arguments);
   },
 
   emerg(message, meta) {
     check(message, String);
-    if (this._shouldLog()) this._logger.emerg.apply(this, arguments);
+    if (this._isRunning()) this._logger.emerg.apply(this, arguments);
   },
 
-  _shouldLog() {
+  _levelCode(name) {
+    let code = {
+      'emerg': 0,
+      'alert': 1,
+      'crit': 2,
+      'error': 3,
+      'warning': 4,
+      'notice': 5,
+      'info':6,
+      'debug': 7
+    }
+    return code[name];
+  },
+
+  _isRunning() {
     if (this._state === 'running') return true;
   }
 
@@ -86,6 +112,7 @@ Space.Logger = Space.Object.extend(Space, 'Logger', {
 // System log
 Space.log = new Space.Logger();
 
-if (Space.configuration.sysLog.enabled) {
+if (config.log.enabled) {
+  Space.log.setMinLevel(config.log.minLevel);
   Space.log.start();
 }
