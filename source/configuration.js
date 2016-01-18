6c2473dc8993d37601f66f79f5d4fd19b6124c06
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-18 17:50:54 +1100

    Guards and provides default value for client-only scenario
    Prior to this client-only tests would fail since the implementation was
    expecting the server to define the logging config. This fix simply
    guards and sets the same defaults if space:base is only loaded on the
    client. The style matches the existing

--- source/configuration.js
+++ source/configuration.js
@@ -28,10 +28,13 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
 
+  let log = Meteor.settings.public.log;
+
+  // Guard and defaults when not loaded on server
   Space.configuration = {
     log: {
-      enabled: Meteor.settings.public.log.enabled,
-      minLevel: Meteor.settings.public.log.minLevel
+      enabled: log && log.enabled || false,
+      minLevel: log && log.minLevel || 'info'
     }
   };
 }
