f165a99a945a5dc14a5cc6dcff79ccbf8b27c8de
qejk <qejk@qejk.net>
2017-01-10 16:40:57 +0100

    Adds index

---
+++ source/index.js
@@ -0,0 +1,35 @@
+import Space from './space.js';
+import SpaceObject from './object.coffee';
+import Struct from './struct.coffee';
+import SpaceError from './error.js';
+import {Injector, InjectionError} from './injector.js';
+import Logger from './logger.js';
+import LoggingAdapter from './loggers/adapter.js';
+import ConsoleLogger from './loggers/console-adapter.js';
+import Module from './module.js';
+import Application from './application.js';
+
+Space.Object = SpaceObject;
+Space.Struct = Struct;
+Space.Error = SpaceError;
+Space.Injector = Injector;
+Space.InjectionError = InjectionError;
+Space.Logger = Logger;
+Space.LoggingAdapter = LoggingAdapter;
+Space.ConsoleLogger = ConsoleLogger;
+Space.Module = Module;
+Space.Application = Application;
+
+export {
+  SpaceObject as SpaceObject,
+  Struct as Struct,
+  SpaceError as SpaceError,
+  Injector as Injector,
+  InjectionError as InjectionError,
+  Logger as Logger,
+  LoggingAdapter as LoggingAdapter,
+  ConsoleLogger as ConsoleLogger,
+  Module as Module,
+  Application as Application,
+  Space as Space
+};
