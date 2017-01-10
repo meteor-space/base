e3af6bcd8b31c83d946132f476794c3c8485b862
qejk <qejk@qejk.net>
2017-01-10 12:37:54 +0100

    Converts injector-annotations

---
+++ source/injector-annotations.js
@@ -0,0 +1,26 @@
+import _ from 'underscore';
+import {isNil} from 'lodash';
+
+this.Space.Dependency = function(propertyName, dependencyId) {
+  return function(target) {
+    const proto = target.prototype;
+    if (proto.dependencies && !proto.hasOwnProperty('Dependencies')) {
+      proto.dependencies = _.clone(proto.dependencies);
+    }
+    if (isNil(proto.dependencies)) {proto.dependencies = {};}
+    proto.dependencies[propertyName] = dependencyId || propertyName;
+    return target;
+  };
+};
+
+this.Space.RequireModule = function(moduleId) {
+  return function(target) {
+    const proto = target.prototype;
+    if (proto.requiredModules && !proto.hasOwnProperty('RequiredModules')) {
+      proto.requiredModules = _.clone(proto.requiredModules);
+    }
+    if (isNil(proto.requiredModules)) {proto.requiredModules = [];}
+    proto.requiredModules.push(moduleId);
+    return target;
+  };
+};
