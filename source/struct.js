86ea282dd4ecf89f0e0e76f2c5be16a44181ea3c
qejk <qejk@qejk.net>
2017-01-11 14:27:14 +0100

    Converts Struct

---
+++ source/struct.js
@@ -0,0 +1,37 @@
+import _ from 'underscore';
+import {ensure} from 'simplecheck';
+import {isNil} from 'lodash';
+import SpaceObject from './object.js';
+
+class Struct extends SpaceObject {
+
+  constructor(data = {}) {
+    super(data);
+    this._checkFields(data);
+  }
+
+  fields() {
+    return _.clone(this.constructor.fields) || {};
+  }
+
+  toPlainObject() {
+    const copy = {};
+    for (let key of Object.keys(this.fields())) {
+      if (!isNil(this[key])) {
+        copy[key] = this[key];
+      }
+    }
+    return copy;
+  }
+
+  //  Use the fields configuration to check given data during runtime
+  _checkFields(data) {
+    ensure(data, this.fields());
+  }
+
+  statics: {
+    fields: {}
+  }
+}
+
+export default Struct;
