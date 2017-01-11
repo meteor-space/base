910a51a78196edf596d993d2dd0baedf4fdf4e47
qejk <qejk@qejk.net>
2017-01-11 11:39:04 +0100

    Fixes CircleCI configuration for Mocha for failing tests with updated spacejam@rc

--- ci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export PACKAGE_DIRS='packages'
-
-if [ "$PORT" ]; then
-  spacejam test-packages ./ --driver-package=practicalmeteor:mocha-console-runner --port $PORT
-else
-   spacejam test-packages ./ --driver-package=practicalmeteor:mocha-console-runner
-fi
