ca212f330539ad133367e5fffe66534d70fd3c84
qejk <qejk@qejk.net>
2017-01-12 22:35:44 +0100

    Adds global sinon for testing

--- test/common.js
+++ test/common.js
@@ -1,8 +1,10 @@
 const chai = require('chai');
+const sinon = require('sinon');
 const sinonChai = require("sinon-chai");
 const spaceTesting = require('space-testing');
 
 global.expect = chai.expect;
+global.sinon = sinon;
 
 chai.use(sinonChai);
 chai.use(spaceTesting);
