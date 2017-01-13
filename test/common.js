d3296c465f61f1281b12e4b8afeb284db786784f
qejk <qejk@qejk.net>
2017-01-13 13:46:29 +0100

    Updates common.js to use new modular approach of space-testing

--- test/common.js
+++ test/common.js
@@ -2,10 +2,13 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require("sinon-chai");
 const spaceTesting = require('space-testing');
+const chaiExtensions = spaceTesting.chai;
 
 global.expect = chai.expect;
 global.sinon = sinon;
 
 chai.use(sinonChai);
-chai.use(spaceTesting);
+for (let key in chaiExtensions) {
+  chai.use(chaiExtensions[key]);
+}
 
