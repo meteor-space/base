6853d14e97e351906134ab9d9e44939ade82e598
qejk <qejk@qejk.net>
2017-01-18 17:22:04 +0100

    Changed how new assertions are added to chai
    Figure this out later

--- test/common.js
+++ test/common.js
@@ -1,9 +1,14 @@
+const values = require('lodash/values');
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require("sinon-chai");
-const {spaceChai} = require('space-testing');
+const {extensions} = require('space-testing');
 chai.use(sinonChai);
-chai.use(spaceChai);
+
+for (let extension of values(extensions.chai)) {
+  chai.use(extension);
+}
+
 
 global.expect = chai.expect;
 global.sinon = sinon;
