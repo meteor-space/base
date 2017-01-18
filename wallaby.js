13fac7fe789fa03dbd1cc31ca56c127059a5a563
qejk <qejk@qejk.net>
2017-01-18 16:57:49 +0100

    Adds wallaby.js configuration file with dynamic .babelrc reading for Babel compiler

---
+++ wallaby.js
@@ -0,0 +1,44 @@
+const assign = require('lodash/assign');
+const fs = require('fs');
+
+
+let babelrc = null;
+let babelrcFile = null;
+try {
+  babelrcFile = fs.readFileSync('.babelrc', 'utf8');
+} catch (e) {
+  console.warn(
+    `Babel configuration file '.babelrc' was not found in root directory`
+  );
+}
+if (babelrcFile !== null) {babelrc = JSON.parse(babelrcFile);}
+
+module.exports = function(wallaby) {
+  let babelConfig = {babel: require('babel-core')};
+  if (babelrc) {
+    babelConfig = assign(babelConfig, babelrc);
+  }
+  const babelCompiler = wallaby.compilers.babel(babelConfig);
+  return {
+    files: [
+      {pattern: 'src/**/*.js'}
+    ],
+    tests: [
+      {pattern: 'test/**/*.js'}
+    ],
+    compilers: {
+      '**/*.js': babelCompiler
+    },
+    testFramework: 'mocha',
+    env: {
+      type: 'node',
+      runner: 'node',
+      params: {
+        env: 'NODE_ENV=test'
+      }
+    },
+    delays: {
+      run: 700
+    }
+  };
+};
