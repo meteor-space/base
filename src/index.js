0c1a11820219a06ea9f296704f336b7293f2bb44
qejk <qejk@qejk.net>
2017-01-18 17:03:18 +0100

    Exports all modules

--- src/index.js
+++ src/index.js
@@ -1,35 +1,38 @@
-import Space from './space.js';
-import SpaceObject from './object.js';
 import Struct from './struct.js';
 import SpaceError from './error.js';
-import {Injector, InjectionError} from './injector.js';
+import {Injector, InjectionError, Provider} from './injector.js';
 import Logger from './logger.js';
-import LoggingAdapter from './loggers/adapter.js';
-import ConsoleLogger from './loggers/console-adapter.js';
+import LoggingTransport from './logging-transports/logging-transport.js';
+import ConsoleTransport from './logging-transports/console-transport.js';
 import Module from './module.js';
-import Application from './application.js';
+import App from './app.js';
+import helpers from './helpers.js';
+import dependencies from './decorators/dependencies-decorator.js';
 
-Space.Object = SpaceObject;
-Space.Struct = Struct;
-Space.Error = SpaceError;
-Space.Injector = Injector;
-Space.InjectionError = InjectionError;
-Space.Logger = Logger;
-Space.LoggingAdapter = LoggingAdapter;
-Space.ConsoleLogger = ConsoleLogger;
-Space.Module = Module;
-Space.Application = Application;
+const Space = {
+  Struct: Struct,
+  Error: SpaceError,
+  Injector: Injector,
+  InjectionError: InjectionError,
+  Logger: Logger,
+  LoggingTransport: LoggingTransport,
+  ConsoleTransport: ConsoleTransport,
+  Module: Module,
+  App: App
+};
 
 export {
-  SpaceObject as SpaceObject,
-  Struct as Struct,
-  SpaceError as SpaceError,
-  Injector as Injector,
-  InjectionError as InjectionError,
-  Logger as Logger,
-  LoggingAdapter as LoggingAdapter,
-  ConsoleLogger as ConsoleLogger,
-  Module as Module,
-  Application as Application,
-  Space as Space
+  Struct,
+  SpaceError,
+  Injector,
+  InjectionError,
+  Provider,
+  Logger,
+  LoggingTransport,
+  ConsoleTransport,
+  Module,
+  App,
+  Space,
+  helpers,
+  dependencies
 };
