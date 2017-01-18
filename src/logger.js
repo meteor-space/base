69d222a70727f6ba63db4135f57d06dfc455ab83
qejk <qejk@qejk.net>
2017-01-18 17:00:03 +0100

    Redesigns Logger

--- src/logger.js
+++ src/logger.js
@@ -1,102 +1,174 @@
-import SpaceObject from './object.js';
-import {values as ObjectValues} from 'lodash';
-const Logger = SpaceObject.extend('Space.Logger', {
+import {isNil} from 'lodash';
 
-  STATES: {
+class Logger {
+
+  static ERRORS = {
+    mappingExists(id) {
+      return `Transport with id '${id}' would be overriden. To override existing
+      mapping use <Logger.prototype.overrideTransport>`;
+    },
+    invalidId: 'Cannot map <null> or <undefined>'
+  }
+
+  STATES = {
     stopped: 'stopped',
     running: 'running'
-  },
+  }
 
-  Constructor() {
+  /**
+   * Creates a Logger.
+   */
+  constructor() {
     this._state = this.STATES.stopped;
-    this._adapters = {};
-  },
+    this._transports = {};
+  }
 
-  addAdapter(id, adapter, shouldOverride = false) {
-    if (!id || typeof id !== 'string') {
+  /**
+   * Starts logging.
+   */
+  start() {
+    this._state = this.STATES.running;
+  }
+
+  /**
+   * Stops logging.
+   */
+  stop() {
+    this._state = this.STATES.stopped;
+  }
+
+  /**
+   * Adds transport to logger.
+   * @param {String}  id  Not yet taken id for transport.
+   * @param {LoggingTransport|*} transport Logging transport.
+   * @param {Boolean} [shouldOverride=false] Flag indicating that transport
+   * should be overriden.
+   * @throws {Error} Will throw an error if the id argument is not a string.
+   * @throws {Error} Will throw an error if the transport would override
+   * existing one.
+   */
+  addTransport(id, transport, shouldOverride = false) {
+    if (isNil(id) || !(id && transport)) {
       throw new Error(this.constructor.ERRORS.invalidId);
     }
-    if (this.hasAdapter(id) && !shouldOverride) {
+    if (this.hasTransport(id) && !shouldOverride) {
       throw new Error(this.constructor.ERRORS.mappingExists(id));
     }
-    this._adapters[id] = adapter;
-  },
+    this._transports[id] = transport;
+  }
 
-  overrideAdapter(id, adapter) {
-    return this.addAdapter(id, adapter, true);
-  },
+  /**
+   * Override existing transport.
+   * @param  {String} id        Transport's mapping id.
+   * @param  {LoggingTransport|*} transport Logging transport.
+   */
+  overrideTransport(id, transport) {
+    this.addTransport(id, transport, true);
+  }
 
-  getAdapter(id) {
-    return this._adapters[id] || null;
-  },
+  /**
+   * Returns transport.
+   * @param  {String} id Transport's id.
+   * @return {LoggingTransport|*)
+   */
+  getTransport(id) {
+    return this._transports[id] || null;
+  }
 
-  hasAdapter(id) {
-    return (this._adapters[id] !== null && this._adapters[id] !== undefined);
-  },
+  /**
+   * Evaluates if transport exists.
+   * @param  {String}  id  Transport's mapping id.
+   * @return {Boolean}
+   */
+  hasTransport(id) {
+    return (!isNil(this._transports[id]));
+  }
 
-  removeAdapter(id) {
-    if (this._adapters[id]) {delete this._adapters[id];}
-  },
+  /**
+   * Removes transport from logger's transports.
+   * @param  {String} id Transport's id.
+   */
+  removeTransport(id) {
+    if (this._transports[id]) {delete this._transports[id];}
+  }
 
-  getAdapters() {
-    return this._adapters;
-  },
+  getTransports() {
+    return Object.values(this._transports);
+  }
 
-  start() {
-    if (this.isInState(this.STATES.stopped)) {
-      this._state = this.STATES.running;
-    }
-  },
+  getMappings() {
+    return this._transports;
+  }
 
-  stop() {
-    if (this.isInState(this.STATES.running)) {
-      this._state = this.STATES.stopped;
-    }
-  },
+  /**
+   * Evaluates if logger is in state.
+   * @param  {String}  expectedState One of available states.
+   * @return {Boolean}
+   */
+  hasState(expectedState) {
+    return (this._state === expectedState);
+  }
 
+  /**
+   * Evaluates if logger is in 'running' state.
+   * @return {Boolean}
+   */
+  isRunning() {
+    return this.hasState(this.STATES.running);
+  }
+
+  /**
+   * Evaluates if logger is in 'stopped' state.
+   * @return {Boolean}
+   */
+  isStopped() {
+    return this.hasState(this.STATES.stopped);
+  }
+
+  /**
+   * Logs message with 'debug' level.
+   * @param  {...*} args Any type that transport supports.
+   */
   debug(...args) {
     this._log('debug', args);
-  },
+  }
 
+  /**
+   * Logs message with 'info' level.
+   * @param  {...*} args Any type that transport supports.
+   */
   info(...args) {
     this._log('info', args);
-  },
+  }
 
+  /**
+   * Logs message with 'warning' level.
+   * @param  {...*} args Any type that transport supports.
+   */
   warning(...args) {
     this._log('warning', args);
-  },
+  }
 
+  /**
+   * Logs message with 'error' level.
+   * @param  {...*} args Any type that transport supports.
+   */
   error(...args) {
     this._log('error', args);
-  },
+  }
 
-  isInState(expectedState) {
-    return (this._state === expectedState);
-  },
-
-  isRunning() {
-    return this.isInState(this.STATES.running);
-  },
-
-  isStopped() {
-    return this.isInState(this.STATES.stopped);
-  },
-
+  /**
+   * Logs message with level.
+   * @param  {String} level Supported logging level by logging library.
+   * @param  {...*} args Any type that transport supports.
+   */
   _log(level, args) {
-    if (!this.isInState(this.STATES.running)) {return;}
+    if (!this.hasState(this.STATES.running)) {return;}
 
-    for (let adapter of ObjectValues(this.getAdapters())) {
-      adapter[level].apply(adapter, args);
+    for (let adapter of this.getTransports()) {
+      adapter[level](...args);
     }
   }
-});
-
-Logger.ERRORS =  {
-  mappingExists(id) {
-    return `Adapter with id '${id}' would be overwritten. Use method
-    'overrideAdapter' for that`;
-  },
-  invalidId: 'Cannot map <null> or <undefined> or non string values'
-};
+}
 
 export default Logger;
