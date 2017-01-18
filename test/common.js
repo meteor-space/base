45170f819976666ac676a635e1a9c3935dcbcc3e
qejk <qejk@qejk.net>
2017-01-18 17:02:44 +0100

    Updates Mocha’s common.js to use new space-testing modular approach

--- test/common.js
+++ test/common.js
@@ -1,14 +1,10 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require("sinon-chai");
-const spaceTesting = require('space-testing');
-const chaiExtensions = spaceTesting.chai;
+const {spaceChai} = require('space-testing');
+chai.use(sinonChai);
+chai.use(spaceChai);
 
 global.expect = chai.expect;
 global.sinon = sinon;
-
-chai.use(sinonChai);
-for (let key in chaiExtensions) {
-  chai.use(chaiExtensions[key]);
-}
 
